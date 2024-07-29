971d08d3b987a6c9316448cd9af1be12317e9d4f
AndJoo19 <andyjoo@hotmail.com>
2024-07-29 17:47:05 -0500

    Added license

--- obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
+++ obj/Debug/net8.0/CatWorx.BadgeMaker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3916103b6741153116aff6d18d425c11b2556b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatWorx.BadgeMaker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
